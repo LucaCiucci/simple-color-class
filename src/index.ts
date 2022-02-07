cd33b1b3137f21f52b11921cd91f6d6bce22d3d3
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2022-02-07 22:42:57 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 
+// https://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hexadecimal-in-javascript
+function decimalToHex(d : number, padding : number) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+        hex = "0" + hex;
+    }
+
+    return hex;
+}
+
 export class Color {
 	public r : number = 0;
 	public g : number = 0;
